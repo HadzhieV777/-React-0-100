e7e4811a2392566b020db71aa960e355ad2dcb60
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-12 15:27:35 +0300

    Added jokesData.js

---
+++ jokes-app/src/jokesData.js
@@ -0,0 +1,48 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    setup: "What did the elephant say to the naked man?",
+    punchline: "How do you breathe through that [tiny thing]?",
+  },
+
+  {
+    setup: "A man goes to the zoo and the only animal there is one dog.",
+    punchline: "It is a shitzhu.",
+  },
+
+  {
+    setup: "I told my wife she was drawing her eyebrows too high.",
+    punchline: "She looked surprised.",
+  },
+
+  {
+    setup: "Three guys walk into a bar.",
+    punchline: "You'd think one of them would've seen it.",
+  },
+
+  {
+    setup: "My wife called me immature.",
+    punchline: "I told her to get outta my fort.",
+  },
+
+  {
+    setup: "Two cannibals are eating a clown.",
+    punchline:
+      "One looks at the other and asks, 'Does this taste funny to you?'",
+  },
+
+  {
+    setup: "How do you find Will Smith in the snow?",
+    punchline: "You look for the fresh prints.",
+  },
+
+  {
+    setup: "Outside of a dog, a book is a man's best friend.",
+    punchline: "Inside a dog, it's too dark to read.",
+  },
+
+  {
+    setup: "Two drums and a cymbal fall off a cliff...",
+    punchline: "Ba-dum tish!",
+  }
+];
