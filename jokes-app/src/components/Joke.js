989d932c79c61b3c82be90b4eedbd55961dfe97b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-12 13:53:26 +0300

    Added components dir

---
+++ jokes-app/src/components/Joke.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Joke(props) {
+  return (
+    <div>
+      {/* <h3 style={{display: props.setup ? "block" : "none"}}>Setup: {props.setup}</h3> */}
+      {props.setup && <h3>Setup: {props.setup}</h3>}
+      <p>Punchline: {props.punchline}</p>
+      <hr />
+    </div>
+  );
+}
