53452e4b5c79a0390275df017732afc1e863b6a9
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-15 11:07:55 +0300

    Added data.js

---
+++ airbnb-experiences/src/data.js
@@ -0,0 +1,47 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    _id: 1,
+    title: "Life lessons with Katie Zaferes",
+    description:
+      'I will share with you what I call "Positively impactful moments of disapointment. " Throughout my career, many of my highest moments only came after losses. But learning from my losses is what gave me the ability to reach my goals.',
+    price: 136,
+    coverImg: "card-image.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+    location: "Online",
+    openSpots: 0,
+  },
+
+  {
+    _id: 2,
+    title: "Learn Wedding Photography",
+    description:
+      "Interested in becoming a wedding potographer? For beginner and experienced photographers alike join us in learning techniques required to leave the happy couple with memories that will last a lifetime.",
+    price: 125,
+    coverImg: "wedding-photography.png",
+    stats: {
+      rating: "5.0",
+      reviewCount: "30",
+    },
+    location: "Online",
+    openSpots: "27",
+  },
+
+  {
+    _id: 3,
+    title: "Group Mountain Biking",
+    description:
+      "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+    price: 50,
+    coverImg: "mountain-bike.png",
+    stats: {
+      rating: "5.0",
+      reviewCount: "22",
+    },
+    location: "Norway, Oslo",
+    openSpots: "18",
+  },
+];
