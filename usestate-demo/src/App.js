daf4785c89d8c36034451449a80e9ec94dd4360e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-16 11:16:06 +0300

    Added src/App.js

---
+++ usestate-demo/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function App() {
+  let [count, setCount] = React.useState(0);
+  /**
+   * Note: if you ever need the old value of state
+   * to help you determine the new value of state,
+   * you should pass a callback function to your
+   * state setter function instead of using state
+   * directly. This callback function will receive
+   * the old value of state as its parameter,
+   * which you can then use to determine your new
+   * value of state
+   */
+
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function subtract() {
+    count >= 1 && setCount((prevCount) => prevCount - 1);
+  }
+
+  return (
+    <div className="counter">
+      <h1>Press the buttons to change the counter!</h1>
+      <button className="counter-minus" onClick={subtract}>
+        -
+      </button>
+      <div className="counter-count">
+        <h1>{count}</h1>
+      </div>
+      <button className="counter-plus" onClick={add}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
+// 06: 05: 11
