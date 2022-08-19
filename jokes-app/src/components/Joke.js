9a21d08fc3969aa97de436576af9c7ef95649ede
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-19 14:17:28 +0300

    Edited Joke.js

--- jokes-app/src/components/Joke.js
+++ jokes-app/src/components/Joke.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 export default function Joke(props) {
+  const [isShown, setIsShown] = React.useState(false);
+
+  function toggleShown() {
+    setIsShown((prevShown) => !prevShown);
+  }
   return (
     <div>
       {/* <h3 style={{display: props.setup ? "block" : "none"}}>Setup: {props.setup}</h3> */}
-      {props.setup && <h3>Setup: {props.setup}</h3>}
-      <p>Punchline: {props.punchline}</p>
+      {props.setup && <h3>{props.setup}</h3>}
+      {isShown && <p>{props.punchline}</p>}
+      <button onClick={toggleShown}>{isShown ? "Hide": "Show"} Punchline</button>
       <hr />
     </div>
   );
